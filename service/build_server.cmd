c61bcea43f597adeaa27a90084c1d5270a062044
Oleksandr Palamarchuk <alex.y.palamarchuk@gmail.com>
2020-09-06 21:07:04 +0300

    service int

---
+++ service/build_server.cmd
@@ -0,0 +1,5 @@
+set ANT_HOME=C:\env\apache-ant-1.9.15
+set JAVA_HOME=C:\Progra~1\Java\jdk-13.0.1
+set AXIS2_HOME=C:\Users\opal\Projects\Axis2SimpleApp1\axis2-1.7.9
+
+%ANT_HOME%\bin\ant.bat generate.service
